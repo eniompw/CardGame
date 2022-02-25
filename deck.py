5998034e28d8911a61d81cab2ad8303ad6432d48
eniompw <71254710+eniompw@users.noreply.github.com>
2022-02-25 15:02:17 +0000

    Create deck.py

---
+++ deck.py
@@ -0,0 +1,74 @@
+import random
+
+class TCard():
+  def __init__(self,Suit,Rank):
+    self.Suit = Suit
+    self.Rank = Rank
+
+def GetRank(RankNo):
+  Rank = ''
+  if RankNo == 1:
+    Rank = 'Ace'
+  elif RankNo == 2:
+    Rank = 'Two'
+  elif RankNo == 3:
+    Rank = 'Three'
+  elif RankNo == 4:
+    Rank = 'Four'
+  elif RankNo == 5:
+    Rank = 'Five'
+  elif RankNo == 6:
+    Rank = 'Six'
+  elif RankNo == 7:
+    Rank = 'Seven'
+  elif RankNo == 8:
+    Rank = 'Eight'
+  elif RankNo == 9:
+    Rank = 'Nine'
+  elif RankNo == 10:
+    Rank = 'Ten'
+  elif RankNo == 11:
+    Rank = 'Jack'
+  elif RankNo == 12:
+    Rank = 'Queen'
+  elif RankNo == 13:
+    Rank = 'King'
+  return Rank
+
+def GetSuit(SuitNo):
+  Suit = ''
+  if SuitNo == 1:
+    Suit = 'Clubs'
+  elif SuitNo == 2:
+    Suit = 'Diamonds'
+  elif SuitNo == 3:
+    Suit = 'Hearts'
+  elif SuitNo == 4:
+    Suit = 'Spades'
+  return Suit
+
+def CreateDeck(Deck):
+    for S in range(1,5):
+        for R in range(1,14):
+            Deck.append(TCard(S,R))
+    return Deck
+
+def ShuffleDeck(Deck):
+  SwapSpace = TCard(0,0)
+  NoOfSwaps = 1000
+  for NoOfSwapsMadeSoFar in range(1, NoOfSwaps + 1):
+    Position1 = random.randint(0, 51)
+    Position2 = random.randint(0, 51)
+    SwapSpace.Rank = Deck[Position1].Rank
+    SwapSpace.Suit = Deck[Position1].Suit
+    Deck[Position1].Rank = Deck[Position2].Rank
+    Deck[Position1].Suit = Deck[Position2].Suit
+    Deck[Position2].Rank = SwapSpace.Rank
+    Deck[Position2].Suit = SwapSpace.Suit
+    return Deck
+
+def DisplayCard(ThisCard):
+  return(GetRank(ThisCard.Rank),GetSuit(ThisCard.Suit))
+
+def GetCard(Deck, index):
+    return DisplayCard(Deck[index])
