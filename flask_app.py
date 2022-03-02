fd827c5087546b554125995b25e7fdfe3fd8a046
eniompw <71254710+eniompw@users.noreply.github.com>
2022-03-02 17:10:20 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 import deck     # from deck.py
 
 Deck = []
-index = -1
+index = 0
 Deck = deck.CreateDeck(Deck)
 Deck = deck.ShuffleDeck(Deck)
 
 @app.route('/')
 def home():
     global index
-    index = index + 1
-    return str(index) + " : " + str(deck.GetCard(Deck, index))
+    player = []
+    for i in range(5):
+        player.append(deck.GetCard(Deck, index))
+        index = index + 1
+    return render_template('cards.html', player=player)
